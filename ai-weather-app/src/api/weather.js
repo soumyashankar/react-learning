b038264898c5cf45efc0b71e47f1861ed692785c
Soumya Shankar <soumya1492@gmail.com>
2025-06-24 12:34:12 +0530

    updated endpoint

--- ai-weather-app/src/api/weather.js
+++ ai-weather-app/src/api/weather.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getCityWeather = async (cityName) => {
-  const apiKey = process.env.VITE_WEATHER_API_KEY;
+  const apiKey = process.env.WEATHER_API_KEY;
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
 
   const response = await axios.get(url);
